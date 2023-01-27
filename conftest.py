8e901b640bc871c55ae9b97de13df60b5ac26da0
masa-che <masache1983@gmail.com>
2023-01-27 08:56:13 +0200

    added base_page.py, conftest.py, requirements.txt

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")  # при помощи этой фикстуры и scope function веб драйвер будет открываться и закрываться для каждого файла с тестами
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())      # запуск браузера chrome "setup"
+    driver.maximize_window()                                        # открытие окна браузера на весь экран
+    yield driver                                                    # работает тело теста
+    driver.close()                                                  # "teardown"
---
+++ pages/base_page.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):                       # поиск одного видимого елемента
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):                     # поиск всех видимых елементов
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):                       # ждёт появления элемента в DOM дереве
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def elements_are_present(self, locator, timeout=5):                     # ждёт появления всех элементов в DOM дереве
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):                   # поиск одного невидимого елемента
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):                     # для проверок кликабельности елемента
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):                                       # скролл к выбранному елементу
+        self.driver.execute_script("argument[0].scrollIntoView();", element)
+
+# DOM - Объектная Модель Документа (Document Object Model) – это программный интерфейс (API) для HTML и XML документов
+
