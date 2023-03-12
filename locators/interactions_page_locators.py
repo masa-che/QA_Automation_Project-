4ac3d33d80b3ddbddc5b7e7bb96b75a946c3c70d
masa-che <masache1983@gmail.com>
2023-03-12 16:52:36 +0200

    added classes:   TestSelectablePage added test: - test_selectable

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -7,3 +7,18 @@ class SortablePageLocators:
     LIST_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-list"] div[class="list-group-item list-group-item-action"]')
     GRID_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-grid"] div[class="list-group-item list-group-item-action"]')
 
+
+class SelectablePageLocators:
+    TAB_LIST = (By.CSS_SELECTOR, 'a[id="demo-tab-list"]')
+    # не активный элемент "List"
+    LIST_ITEM = (By.CSS_SELECTOR, 'ul[id="verticalListContainer"]'
+                                  ' li[class="mt-2 list-group-item list-group-item-action"]')
+    # активный с нажатием click, элемент
+    LIST_ITEM_ACTIVE = (By.CSS_SELECTOR, 'ul[id="verticalListContainer"]'
+                                         ' li[class="mt-2 list-group-item active list-group-item-action"]')
+    TAB_GRID = (By.CSS_SELECTOR, 'a[id="demo-tab-grid"]')
+    # не активный элемент "Grid"
+    GRID_ITEM = (By.CSS_SELECTOR, 'div[id="gridContainer"] li[class="list-group-item list-group-item-action"]')
+    # активный с нажатием click, элемент
+    GRID_ITEM_ACTIVE = (By.CSS_SELECTOR, 'div[id="gridContainer"]'
+                                         ' li[class="list-group-item active list-group-item-action"]')
--- pages/interactions_page.py
+++ pages/interactions_page.py
@@ -1,17 +1,18 @@
-from locators.interactions_page_locators import SortablePageLocators
+from locators.interactions_page_locators import SortablePageLocators, SelectablePageLocators
 from pages.base_page import BasePage
 
 import random
+import time
 
 
 class SortablePage(BasePage):
     locators = SortablePageLocators()
 
-    def get_sortable_items(self, elements):                 # метод возврата текста всех значений tab "List"
+    def get_sortable_items(self, elements):               # метод возврата текста всех значений tab "List"
         item_list = self.elements_are_visible(elements)
         return [item.text for item in item_list]
 
-    def change_list_order(self):                            # метод для изменения порядка значений в tab "List"
+    def change_list_order(self):                          # метод для изменения порядка значений в tab "List"
         self.element_is_visible(self.locators.TAB_LIST).click()
         order_before = self.get_sortable_items(self.locators.LIST_ITEM)
         # меняем order_before случайным образом, изменяя положения двух строк в List (random.sample)
@@ -23,7 +24,7 @@ class SortablePage(BasePage):
         order_after = self.get_sortable_items(self.locators.LIST_ITEM)
         return order_before, order_after
 
-    def change_grid_order(self):                            # метод для изменения порядка значений в tab "GRID"
+    def change_grid_order(self):                          # метод для изменения порядка значений в tab "GRID"
         self.element_is_visible(self.locators.TAB_GRID).click()
         order_before = self.get_sortable_items(self.locators.GRID_ITEM)
         item_list = random.sample(self.elements_are_visible(self.locators.GRID_ITEM), k=2)
@@ -32,4 +33,33 @@ class SortablePage(BasePage):
         self.action_drag_and_drop_to_element(item_what, item_where)
         order_after = self.get_sortable_items(self.locators.GRID_ITEM)
         return order_before, order_after
+
+
+class SelectablePage(BasePage):
+    locators = SelectablePageLocators()
+
+    # def click_selectable_item(self, elements):          # метод выбора одного случайного элемента и нажатие на него
+    #     item_list = self.elements_are_visible(elements)
+    #     random.sample(item_list, k=1)[0].click()
+
+    def click_selectable_item(self, elements):            # метод выбора нескольких случайных элементов и нажатие на них
+        item_list = random.sample(self.elements_are_visible(elements), k=random.randint(2, 4))
+        for item in item_list:
+            item.click()
+            time.sleep(0.5)
+
+    def select_list_item(self):                           # метод нажатия на елемент вкладки "List"
+        self.element_is_visible(self.locators.TAB_LIST).click()
+        self.click_selectable_item(self.locators.LIST_ITEM)
+        time.sleep(2)
+        active_element = self.element_is_visible(self.locators.LIST_ITEM_ACTIVE)
+        return active_element.text
+
+    def select_grid_item(self):                           # метод нажатия на елемент вкладки "Grid"
+        self.element_is_visible(self.locators.TAB_GRID).click()
+        self.click_selectable_item(self.locators.GRID_ITEM)
+        active_element = self.element_is_visible(self.locators.GRID_ITEM_ACTIVE)
+        return active_element.text
+
+
 
