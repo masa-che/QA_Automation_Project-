840b800eebf8c0ebfbf99a5cfa0e877e218f1ea6
masa-che <masache1983@gmail.com>
2023-04-08 23:59:18 +0300

    add comment in elements_test.py add functional to conftest.py

--- conftest.py
+++ conftest.py
@@ -6,13 +6,11 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")  # при помощи этой фикстуры и scope function веб драйвер будет открываться и закрываться для каждого файла с тестами
 def driver():
-    webdriver.Chrome(ChromeDriverManager().install())               # запуск браузера chrome "и его обновление"
     options = Options()
-    options.add_argument("user-data-dir=C:\\profile")               # создание профиля в браузере
-    driver = webdriver.Chrome(chrome_options=options)               # запуск браузера с профилем
-    driver.get("https://google.com")                                # первоначальная страница работы браузера
-    driver.maximize_window()                                        # открытие окна браузера на весь экран
-    yield driver                                                    # работает тело теста
-    driver.quit()                                                   # "teardown"
-
+    options.add_argument("user-data-dir=C:\\profile")                            # создание профиля в браузере
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)  # запуск браузра с профилем, обновление
+    driver.get("https://google.com")                                           # первоначальная страница работы браузера
+    driver.maximize_window()                                                   # открытие окна браузера на весь экран
+    yield driver                                                               # работает тело теста
+    driver.quit()                                                              # "teardown"
 
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -64,16 +64,16 @@ class CheckBoxPage(BasePage):
                 break                                                     # выход из цикла
 
     def get_checked_checkboxes(self):
-        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)  # список отм елементов CHECKED_ITEMS
+        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)  # список отм элементов CHECKED_ITEMS
         data = []                                                              # [list] в который будут записаны данные
         for box in checked_list:                                               # все отмеченные элементы из checked_list
             title_item = box.find_element(By.XPATH, self.locators.TITLE_ITEM)  # отбираются по xpath TITLE_ITEM
-            print(title_item.text)                                            # принтуем для сравнения
+            print(title_item.text)                                             # принтуем для сравнения
             data.append(title_item.text)                                       # add в data текст отмеченных checkboxes
         return str(data).replace(' ', '').replace('.doc', '').lower()          # return отформатированный [список] data
 
     def get_output_result(self):
-        result_list = self.elements_are_present(self.locators.OUTPUT_RESULT)   # список елементов OUTPUT_RESULT
+        result_list = self.elements_are_present(self.locators.OUTPUT_RESULT)   # список элементов OUTPUT_RESULT
         data = []                                                              # [list] в который будут записаны данные
         for item in result_list:                                               # все отмеченные элементы из result_list
             # print(item.text)                                                 # принтуем для сравнения
